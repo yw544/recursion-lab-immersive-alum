ac378228d028ab408781db6d55caeecbab4e08a3
York Wang <yw544@nyu.edu>
2019-01-07 05:05:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ function reverseString(myString){
     return reverseString(myString.substring(1)).concat(myString[0]);
   }
 }
+ function isPalindrome(string){
+
+   if (string.length === 1) {
+     return true; 
+   }
+   else {
+     if(string[0]===string[string.length-1])
+     return isPalindrome(string.substring(1, string.length-1));
+   }
+   else {
+     return false;
+   }
+   }
+ }
