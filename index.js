447138aa07d9ea8e3078fb2536a42e9abfbbbfe8
York Wang <yw544@nyu.edu>
2019-01-07 04:25:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ function printString(myString) {
 
 
 function reverseString(myString) {
+  function printString(myString) {
+    console.log(myString[0]);
+
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    }
+     else {
+      return true;
+
+    }
+  }
  let reversed = myString.split("").reverse().join("");
 
 console.log(reversed);
